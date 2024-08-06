316d5859fcbaa2d0db8e83ac549cf950c9b471db
jeongminoyatlee <jeongminlee@yonsei.ac.kr>
2024-08-06 22:50:52 +0900

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ECommerce from '@/components/Dashboard/E-commerce';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ECommerce />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
